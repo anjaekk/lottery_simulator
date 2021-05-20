c75b94807393a21374ec3916fb5d1cd132e5ebb2
An jaegyeong <74139727+anjaekk@users.noreply.github.com>
2021-05-20 16:55:31 +0900

    사용자 번호 입력 기능 추가

--- lottery.py
+++ lottery.py
@@ -1,18 +1,27 @@
 import random
 
+# 사용자 입력 번호
+enter_number = list(map(int, input().split()))
+
+# 45 초과 번호 삭제
+for i in enter_number:
+    if i > 45:
+        enter_number.remove(i)
+
+
+generate_number_list = enter_number
+
 
 # 랜덤 번호 뽑기
-generate_number_list = []
-
-
-def generate_number(n):
-    while len(generate_number_list) < n:
+def generate_number():
+    while len(generate_number_list) < 6:
         num = random.randint(1, 45)
         # 중복 번호 제거
         if num not in generate_number_list:
             generate_number_list.append(num)
     return sorted(generate_number_list)
-print(generate_number(6))
+
+print(generate_number())
 
 
 # 당첨 번호 뽑기
@@ -34,9 +43,23 @@ def winning_number():
 print(winning_number())
 
 
-#  당첨된 번호 수
-def count_matching_number(list_1, list_2):
-    matching_number = set(list_1) & set(list_2)
-    return len(matching_number)
-
-print(count_matching_number(generate_number(6), winning_number()))
+#  당첨확인
+def check(generate_num, winning_num):
+    matching_number = set(generate_num) & set(winning_num[:6])
+    cnt_matching_number = len(matching_number)
+    # 1등(6개 일치)
+    if cnt_matching_number == 6: 
+        return("결과: 6개 일치\n축하합니다! '1등' 당첨입니다.")
+    # 2등(5개 일치 + 보너스 번호 일치)
+    elif cnt_matching_number == 5 and winning_num[6] in generate_num:
+        return("결과: 5개 일치 및 보너스 번호 일치\n축하합니다! '2등' 당첨입니다.")
+    # 3 ~ 5등
+    elif cnt_matching_number == 5:
+        return("결과: 5개 일치\n축하합니다! '3등' 당첨입니다.")
+    elif cnt_matching_number == 4:
+        return("결과: 4개 일치\n축하합니다! '4등' 당첨입니다.")
+    elif cnt_matching_number == 3:
+        return("결과: 3개 일치\n축하합니다! '5등' 당첨입니다.")
+    # 낙첨
+    else:
+        return("아쉽게도 낙첨입니다.")
